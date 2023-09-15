64fdb68a29e969d569d19d0cbb587e118ddc9cca
Scott-Reilly <92846632+Scott-Reilly@users.noreply.github.com>
2023-09-15 20:44:18 +0530

    Corrected pages, added JSON Tables

---
+++ css/CSVtoJSON.py
@@ -0,0 +1,31 @@
+import csv
+import json
+
+# Function to convert CSV to JSON
+def csv_to_json(csv_file, json_file):
+    # Create an empty list to store the data
+    data = []
+
+    # Open the CSV file for reading
+    with open(csv_file, 'r') as csv_file:
+        # Create a CSV reader object
+        csv_reader = csv.DictReader(csv_file)
+
+        # Iterate through each row in the CSV file
+        for row in csv_reader:
+            # Append each row as a dictionary to the data list
+            data.append(row)
+
+    # Open the JSON file for writing
+    with open(json_file, 'w') as json_file:
+        # Use the json.dump() function to write the data as JSON
+        json.dump(data, json_file, indent=4)
+
+# Specify the CSV input file and JSON output file
+csv_input_file = 'Test-Data/merged_data_supplier.csv'
+json_output_file = 'Test-Data/merged_data_supplier.json'
+
+# Call the function to convert CSV to JSON
+csv_to_json(csv_input_file, json_output_file)
+
+print("CSV file has been successfully converted to JSON.")
